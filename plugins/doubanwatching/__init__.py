e88815d6f4143ace90612487a004860c1e3e66df
ptninja <ptninja@ptninja.com>
2025-01-04 18:12:56 -0800

    Test

--- plugins/doubanwatching/__init__.py
+++ plugins/doubanwatching/__init__.py
@@ -23,7 +23,7 @@ class DouBanWatching(_PluginBase):
     # 插件图标
     plugin_icon = "douban.png"
     # 插件版本
-    plugin_version = "1.9.4"
+    plugin_version = "1.9.5"
     # 插件作者
     plugin_author = "honue"
     # 作者主页
