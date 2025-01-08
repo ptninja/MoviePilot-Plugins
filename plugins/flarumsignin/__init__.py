8c840afa4ba6d8f937de2cd1a2172187b30e1472
ptninja <ptninja@ptninja.com>
2025-01-08 09:50:07 -0800

    Fix mistake overwriting douban watching

--- plugins/flarumsignin/__init__.py
+++ plugins/flarumsignin/__init__.py
@@ -1,7 +1,11 @@
 import json
+import io
 import re
 import time
 from datetime import datetime, timedelta
+from dataclasses import dataclass
+import textwrap
+from ruamel.yaml import YAML, YAMLError
 
 import pytz
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -14,6 +18,11 @@ from app.log import logger
 from app.schemas import NotificationType
 from app.utils.http import RequestUtils
 
+@dataclass
+class FlarumSiteConfig:
+    site_name: str
+    site_url: str
+    cookie: str
 
 class FlarumSignin(_PluginBase):
     # 插件名称
@@ -23,7 +32,7 @@ class FlarumSignin(_PluginBase):
     # 插件图标
     plugin_icon = "flarum.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.0.0"
     # 插件作者
     plugin_author = "ptninja"
     # 作者主页
@@ -39,56 +48,60 @@ class FlarumSignin(_PluginBase):
     _enabled = False
     # 任务执行间隔
     _cron = None
-    _cookie = None
     _onlyonce = False
     _notify = False
     _history_days = None
+    _site_configs: List[FlarumSiteConfig] = None
 
     # 定时器
     _scheduler: Optional[BackgroundScheduler] = None
 
     def init_plugin(self, config: dict = None):
+        if not config:
+            return False
+        
         # 停止现有任务
         self.stop_service()
 
-        if config:
-            self._enabled = config.get("enabled")
-            self._cron = config.get("cron")
-            self._cookie = config.get("cookie")
-            self._notify = config.get("notify")
-            self._onlyonce = config.get("onlyonce")
-            self._history_days = config.get("history_days") or 30
+        logger.info(f"Loading config")
+        self._enabled = config.get("enabled")
+        self._cron = config.get("cron")
+        self._notify = config.get("notify")
+        self._onlyonce = config.get("onlyonce")
+        self._history_days = config.get("history_days") or 30
+        self._site_configs = self.__load_configs(config.get("flarum_site_configs"))
 
         if self._onlyonce:
             # 定时服务
             self._scheduler = BackgroundScheduler(timezone=settings.TZ)
             logger.info(f"Flarum 签到服务启动，立即运行一次")
-            self._scheduler.add_job(func=self.__signin, trigger='date',
+            self._scheduler.add_job(func=self.signin_all_sites, trigger='date',
                                     run_date=datetime.now(tz=pytz.timezone(settings.TZ)) + timedelta(seconds=3),
                                     name="Flarum 签到")
             # 关闭一次性开关
             self._onlyonce = False
-            self.update_config({
-                "onlyonce": False,
-                "cron": self._cron,
-                "enabled": self._enabled,
-                "cookie": self._cookie,
-                "notify": self._notify,
-                "history_days": self._history_days,
-            })
+            config["onlyonce"] = False
+            self.update_config(config=config)
 
             # 启动任务
             if self._scheduler.get_jobs():
                 self._scheduler.print_jobs()
                 self._scheduler.start()
 
-    def __signin(self):
+    def signin_all_sites(self):
         """
-        Flarum 签到
+        逐个站点签到。可以考虑参考 sitestatistic 多线程签到
         """
-        res = RequestUtils(cookies=self._cookie).get_res(url="https://invites.fun")
+        logger.info(f"所有签到: {self._site_configs}")
+        for site in self._site_configs:
+            self.signin(site)
+
+    def signin(self, config: FlarumSiteConfig):
+        logger.info(f"开始签到 {config.site_name} ...")
+
+        res = RequestUtils(cookies=config.cookie).get_res(url=config.site_url)
         if not res or res.status_code != 200:
-            logger.error("请求Flarum 错误")
+            logger.error(f"请求 {config.site_name} 错误")
             return
 
         # 获取csrfToken
@@ -113,9 +126,9 @@ class FlarumSignin(_PluginBase):
             return
 
         headers = {
-            "X-Csrf-Token": csrfToken,
-            "X-Http-Method-Override": "PATCH",
-            "Cookie": self._cookie
+            "X-CSRF-Token": csrfToken,
+            "X-HTTP-Method-Override": "PATCH",
+            "Cookie": config.cookie,
         }
 
         data = {
@@ -130,16 +143,16 @@ class FlarumSignin(_PluginBase):
         }
 
         # 开始签到
-        res = RequestUtils(headers=headers).post_res(url=f"https://invites.fun/api/users/{userId}", json=data)
+        res = RequestUtils(headers=headers).post(url=f"{config.site_url}/api/users/{userId}", json=data)
 
         if not res or res.status_code != 200:
-            logger.error("Flarum 签到失败")
+            logger.error(f"{config.site_name} 签到失败")
 
             # 发送通知
             if self._notify:
                 self.post_message(
                     mtype=NotificationType.SiteMessage,
-                    title="【Flarum 签到任务完成】",
+                    title=f"【{config.site_name} 签到任务完成】",
                     text="签到失败，请检查cookie是否失效")
             return
 
@@ -151,17 +164,18 @@ class FlarumSignin(_PluginBase):
         if self._notify:
             self.post_message(
                 mtype=NotificationType.SiteMessage,
-                title="【Flarum 签到任务完成】",
+                title=f"【{config.site_name} 签到任务完成】",
                 text=f"累计签到 {totalContinuousCheckIn} \n"
-                     f"剩余Flarum  {money}")
+                     f"剩余积分 {money}")
 
         # 读取历史记录
         history = self.get_data('history') or []
 
         history.append({
             "date": datetime.today().strftime('%Y-%m-%d %H:%M:%S'),
+            "siteName": config.site_name,
             "totalContinuousCheckIn": totalContinuousCheckIn,
-            "money": money
+            "money": money,
         })
 
         thirty_days_ago = time.time() - int(self._history_days) * 24 * 60 * 60
@@ -194,10 +208,10 @@ class FlarumSignin(_PluginBase):
         """
         if self._enabled and self._cron:
             return [{
-                "id": "InvitesSignin",
+                "id": "FlarumSignin",
                 "name": "Flarum 签到服务",
                 "trigger": CronTrigger.from_crontab(self._cron),
-                "func": self.__signin,
+                "func": self.signin_all_sites,
                 "kwargs": {}
             }]
         return []
@@ -270,7 +284,7 @@ class FlarumSignin(_PluginBase):
                                 'component': 'VCol',
                                 'props': {
                                     'cols': 12,
-                                    'md': 3
+                                    'md': 6
                                 },
                                 'content': [
                                     {
@@ -286,7 +300,7 @@ class FlarumSignin(_PluginBase):
                                 'component': 'VCol',
                                 'props': {
                                     'cols': 12,
-                                    'md': 3
+                                    'md': 6
                                 },
                                 'content': [
                                     {
@@ -298,18 +312,24 @@ class FlarumSignin(_PluginBase):
                                     }
                                 ]
                             },
+                        ]
+                    },
+                    {
+                        'component': 'VRow',
+                        'content': [
                             {
                                 'component': 'VCol',
                                 'props': {
-                                    'cols': 12,
-                                    'md': 6
+                                    'cols': 12
                                 },
                                 'content': [
                                     {
-                                        'component': 'VTextField',
+                                        'component': 'VAceEditor',
                                         'props': {
-                                            'model': 'cookie',
-                                            'label': 'Flarum cookie'
+                                            'modelvalue': 'flarum_site_configs',
+                                            'lang': 'yaml',
+                                            'theme': 'monokai',
+                                            'style': 'height: 25rem'
                                         }
                                     }
                                 ]
@@ -343,13 +363,12 @@ class FlarumSignin(_PluginBase):
             "enabled": False,
             "onlyonce": False,
             "notify": False,
-            "cookie": "",
             "history_days": 30,
-            "cron": "0 9 * * *"
+            "cron": "0 9 * * *",
+            "flarum_site_configs": self.__get_demo_config()
         }
 
     def get_page(self) -> List[dict]:
-        # 查询同步详情
         historys = self.get_data('history')
         if not historys:
             return [
@@ -382,6 +401,10 @@ class FlarumSignin(_PluginBase):
                             'class': 'whitespace-nowrap break-keep text-high-emphasis'
                         },
                         'text': history.get("date")
+                    },
+                    {
+                        'component': 'td',
+                        'text': history.get("siteName")
                     },
                     {
                         'component': 'td',
@@ -427,6 +450,13 @@ class FlarumSignin(_PluginBase):
                                                 'props': {
                                                     'class': 'text-start ps-4'
                                                 },
+                                                'text': '站点'
+                                            },
+                                            {
+                                                'component': 'th',
+                                                'props': {
+                                                    'class': 'text-start ps-4'
+                                                },
                                                 'text': '连续签到次数'
                                             },
                                             {
@@ -434,7 +464,7 @@ class FlarumSignin(_PluginBase):
                                                 'props': {
                                                     'class': 'text-start ps-4'
                                                 },
-                                                'text': '剩余Flarum '
+                                                'text': '剩余积分'
                                             },
                                         ]
                                     },
@@ -450,7 +480,51 @@ class FlarumSignin(_PluginBase):
             }
         ]
 
-        pass
+    def __load_configs(self, config_str: Optional[str]) -> List[FlarumSiteConfig]:
+        """加载YAML配置字符串，并构造 FlarumSiteConfig 列表。
+
+        Args:
+        config_str (str): 配置内容的字符串。
+
+        Returns:
+        List[ClassifierConfig]: 从配置字符串解析出的配置列表。
+        """
+        if not config_str:
+            return []
+
+        yaml = YAML(typ="safe")
+        try:
+            data = yaml.load(io.StringIO(config_str))
+            return [FlarumSiteConfig(**item) for item in data]
+        except YAMLError as e:
+            self.__log_and_notify_error(f"YAML parsing error: {e}")
+            return []  # 返回空列表或根据需要做进一步的错误处理
+        except Exception as e:
+            self.__log_and_notify_error(f"Unexpected error during YAML parsing: {e}")
+            return []  # 处理任何意外的异常，返回空列表或其它适当的错误响应
+
+    def __log_and_notify_error(self, message):
+        """
+        记录错误日志并发送系统通知
+        """
+        logger.error(message)
+        self.systemmessage.put(message, title="Flarum 论坛签到")
+        
+    @staticmethod
+    def __get_demo_config():
+        """获取默认配置"""
+
+        block = """\
+            ####### 配置说明 BEGIN #######
+            - site_name: invites
+              site_url: https://invites.fun
+              cookie: xxx
+              
+            - site_name: hddolby
+              site_url: https://forums.orcinusorca.org
+              cookie: yyy
+        """
+        return textwrap.dedent(block)
 
     def stop_service(self):
         """
