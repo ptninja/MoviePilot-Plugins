43a0ab416ca5d499c89b556292692b60ed80e5d8
ptninja <ptninja@ptninja.com>
2025-01-22 10:22:14 -0800

    Bump DoubanWatching version to 1.9.6

--- plugins/doubanwatching/__init__.py
+++ plugins/doubanwatching/__init__.py
@@ -41,7 +41,7 @@ class DouBanWatching(_PluginBase):
     # 插件图标
     plugin_icon = "douban.png"
     # 插件版本
-    plugin_version = "1.9.5"
+    plugin_version = "1.9.6"
     # 插件作者
     plugin_author = "honue"
     # 作者主页
